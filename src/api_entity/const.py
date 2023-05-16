401d5064fab034793582b6273d3de5399300d255
Boyarkina Darya <bo_dary@mail.ru>
2023-05-16 23:43:37 +0700

    For junior

--- src/api_entity/const.py
+++ src/api_entity/const.py
@@ -1,12 +1,8 @@
 """
     Константы для API сущностей
 """
-import jwt
 import dpath
 from dataclasses import dataclass
-from pydantic.types import UUID4
-
-from src.api_entity.api_path import ModuleName
 
 
 @dataclass
@@ -20,3 +16,18 @@ class UserCredential:
 if __name__ == "__main__":
     # для теста
     pass
+    import os
+    from src.const import EnvName
+    import src.config
+
+
+    class UserCredential1:
+        def __init__(self, email, password, token=None):
+            self.email = email
+            self.password = password
+            self.token = token
+
+
+    user1_credential = UserCredential1(email="demo1", password="demo2")
+    print(user1_credential.email)
+    print(user1_credential.password)
