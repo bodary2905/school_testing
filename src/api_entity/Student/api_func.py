7308dcbe9eff8fa70ff63cf37f49536eab19ed3d
Boyarkina Darya <bo_dary@mail.ru>
2023-06-02 11:53:00 +0700

    Add comment in api_func for Student

--- src/api_entity/Student/api_func.py
+++ src/api_entity/Student/api_func.py
@@ -27,6 +27,7 @@ class StudentApiFunc:
         """Получаем student_body через get"""
         response = send_get(url=StudentFullPath.get.value / student_id, **kwargs)  # в kwargs передаем headers
         assert response.status_code == 200, f"Wrong status_code {entity_name}:get"
+        # получаем body из ответа в виде словаря
         body = get_response_body(response, err_msg=f"{entity_name}:get")
         # валидируем (создаем экземпляр модели)
         model = StudentModel_get_for_response.parse_obj(body)
