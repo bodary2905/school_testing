a67621e94504e45673febd3036fcd863eff3a900
Boyarkina Darya <bo_dary@mail.ru>
2023-06-02 12:22:35 +0700

    Add register and login users

--- scripts/user_create/user_login.py
+++ scripts/user_create/user_login.py
@@ -1,45 +1,32 @@
 import requests
-import os
-
-# a = os.getcwd()
-# b = os.path.dirname(a)
-# # c = os.path.join(os.getcwd(), "user_create", "users.txt")
-# d = os.path.join(b, "user_create", "users.txt")
-#
-# print(a)
-# print(b)
-# # print(f"{c}")
-# print(d)
+import json
 
 api_url = "http://127.0.0.1:5000/api/v1/auth/login"
 
 usernames = []
 tokens = []
 
-# with open(f"{d}") as file:
-with open("users.txt", 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        line = line.strip()
-        line_split = line.split(",")
-        username = line_split[0].strip()
-        password = line_split[1].strip()
-        body_request = {
-            "username": username,
-            "password": password
-        }
+# открываем json-файл
+with open("users.json", "r") as file:
+    # получаем словарь с юзерами
+    users = json.load(file)
+    # через цикл for логинем user-ов
+    for user in users:
+        response = requests.post(api_url, json=users[user])
+        assert response.status_code == 200, "Wrong status_code during login"
+        body = response.json()  # получаем ответ в виде json
+        tokens.append(f"{body['token']}")  # добавляем токены в список
+        usernames.append(users[user]["username"])
 
-        response_login = requests.post(api_url, json=body_request)  # получаем объект response
-        assert response_login.status_code == 200, "Wrong status_code during login"
-        body_response_login = response_login.json()  # получаем ответ в виде json
-        tokens.append(f"{body_response_login['token']}")
-        usernames.append(username)
-
-with open("tokens.txt", "w") as file:
+token_dict = {}
+with open("tokens.json", "w") as file:
     for username, token in zip(usernames, tokens):
-        file.write(f"{username}: {token}\n")
-
-print(tokens)
+        token_dict[username] = token  # заполняем словарь
+    token_json = json.dumps(token_dict, indent=4)  # преобразуем словарь в json с отступом=4
+    file.write(token_json)  # записываем в файл словарь json
 
 if __name__ == "__main__":
-    pass
+    import os
+
+    filename = os.path.basename(__file__)
+    print(f"{filename} worked success!")
