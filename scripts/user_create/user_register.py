81419eb93182267a3afa8978d03715d2c0507fc7
Boyarkina Darya <bo_dary@mail.ru>
2023-06-02 12:22:35 +0700

    Add register and login users

--- scripts/user_create/user_register.py
+++ scripts/user_create/user_register.py
@@ -3,14 +3,16 @@ import json
 
 api_url = "http://127.0.0.1:5000/api/v1/auth/register"
 # открываем json-файл
-file = open("users.json")
-# получаем словарь с юзерами
-users = json.load(file)
-
-# через цикл for создаем user-ов
-for user in users:
-    response = requests.post(api_url, json=users[user])
-    assert response.status_code == 201, "Wrong status_code during register"
+with open("users.json", "r") as file:
+    # получаем словарь с юзерами
+    users = json.load(file)
+    # через цикл for создаем user-ов
+    for user in users.values():
+        response = requests.post(api_url, json=user)
+        assert response.status_code == 201, "Wrong status_code during register"
 
 if __name__ == "__main__":
-    pass
+    import os
+
+    filename = os.path.basename(__file__)
+    print(f"{filename} worked success!")
