541596426743887a1bb967340aab4e5ab2b1c77a
Boyarkina Darya <bo_dary@mail.ru>
2023-06-02 14:44:22 +0700

    Add student_create and json-file with ids

---
+++ scripts/user_create/student_create.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import random
+
+api_url = "http://127.0.0.1:5000/api/v1/students"
+
+student_ids = []
+email_addresses = []
+with open("tokens.json", "r") as file:
+    # получаем словарь с токенами
+    tokens = json.load(file)
+    # получаем случайный токен из словаря tokens
+    token = random.choice(list(tokens.values()))
+    # формируем хэдере авторизации
+    auth = {"Authorization": token}
+    with open("students.json", "r") as file2:
+        students = json.load(file2)
+        for student in students.values():
+            response = requests.post(api_url, json=student, headers=auth)
+            assert response.status_code == 201, "Wrong status_code during create_student"
+            body = response.json()
+            student_ids.append(body["student_id"])
+            email_addresses.append(body["email_address"])
+
+student_id_dict = {}
+with open("student_ids.json", "w") as file:
+    for email_address, student_id in zip(email_addresses, student_ids):
+        student_id_dict[email_address] = student_id  # заполняем словарь
+    student_id_json = json.dumps(student_id_dict, indent=4)  # преобразуем словарь в json с отступом=4
+    file.write(student_id_json)  # записываем в файл словарь json
+
+if __name__ == "__main__":
+    pass
