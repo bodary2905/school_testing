bdc96a667c1a90f6928230341a0f0a4c050eaec5
Boyarkina Darya <bo_dary@mail.ru>
2023-06-02 11:59:53 +0700

    Add user_reg

--- scripts/user_create/user_register.py
+++ scripts/user_create/user_register.py
@@ -1,19 +1,16 @@
 import requests
+import json
 
 api_url = "http://127.0.0.1:5000/api/v1/auth/register"
+# открываем json-файл
+file = open("users.json")
+# получаем словарь с юзерами
+users = json.load(file)
 
-with open("users.txt") as file:
-    lines = file.readlines()
-    for line in lines:
-        line = line.strip()
-        line_split = line.split(",")
-        body_request = {
-            "username": line_split[0].strip(),
-            "password": line_split[1].strip()
-        }
-        response_register = requests.post(api_url, json=body_request)  # получаем объект response
-        assert response_register.status_code == 201, "Wrong status_code during register"
-        body_response_register = response_register.json()  # получаем ответ в виде json
+# через цикл for создаем user-ов
+for user in users:
+    response = requests.post(api_url, json=users[user])
+    assert response.status_code == 201, "Wrong status_code during register"
 
 if __name__ == "__main__":
     pass
