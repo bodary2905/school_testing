9d5bc89ac941c865250878ebb8768b3e9e84f754
Boyarkina Darya <bo_dary@mail.ru>
2023-06-04 14:13:26 +0700

    Refactor Teacher model

--- src/api_entity/Teacher/model.py
+++ src/api_entity/Teacher/model.py
@@ -26,7 +26,7 @@ class TeacherModel_create_for_response(TeacherModel_create_for_factory):
     staff_id: str = Field(..., description="id-к учителя")
 
     @validator("staff_id")
-    def staff_id_check(cls, v:str):
+    def staff_id_check(cls, v: str):
         assert v.startswith("TC")
         try:
             v_list = v.split("TC")
@@ -34,27 +34,43 @@ class TeacherModel_create_for_response(TeacherModel_create_for_factory):
         except ValueError as e:
             raise ValueError(f"Error in func TeacherModel_create:staff_id") from e
         return v
+
     @validator("email_address")
     def email_address_check(cls, v: str):
         if len(v) > 255:
-             raise ValueError(f"Error in validate TeacherModel_create:email_address_check_len")
+            raise ValueError(f"Error in validate TeacherModel_create:email_address_check_len")
         if len(v) > 129:
-                if not re.fullmatch(r'[\w.-]+@[\w-]+\.[\w.]+', v):
-                    # re.fullmatch(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', v)
-                    raise ValueError(f"Email is invalid")
+            if not re.fullmatch(r'[\w.-]+@[\w-]+\.[\w.]+', v):
+                # re.fullmatch(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', v)
+                raise ValueError(f"Email is invalid")
         else:
             try:
                 validate_email(v, check_deliverability=False)
             except EmailNotValidError as e:
                 print(str(e))
         return v
+
+
 class TeacherModel_update_for_factory(BaseModel):
     first_name: Optional[constr(min_length=1, max_length=50)] = Field(description="имя")
     last_name: Optional[constr(min_length=1, max_length=50)] = Field(description="фамилия")
 
+
+class TeacherModel_update_for_response(TeacherModel_create_for_response):
+    pass
+
+
 class TeacherModel_get_for_response(TeacherModel_create_for_response):
     pass
 
+
+class TeacherModel_delete_for_response(BaseModel):
+    message: str = Field(description="сообщение об успехе удаления")
+
+    @validator("message")
+    def message_check(cls, v: str):
+        assert "You have successfully deleted the teacher with the following ID: TC" in v, f"Wrong message for delete"
+        return v
 
 
 if __name__ == "__main__":
@@ -64,6 +80,7 @@ if __name__ == "__main__":
         "email_address": "test@mail.ru",
         "staff_id": "TC123"
     }
-    teaher_1 = TeacherModel_get_for_response.parse_obj(body)
-
+    body_2 = {"message": "You have successfully deleted the teacher with the following ID: TC"}
+    response = TeacherModel_delete_for_response.parse_obj(body_2)
+    print(response)
     pass
