15b24eafa87cb9bfe67e702b3802f35c9e299dc1
Boyarkina Darya <bo_dary@mail.ru>
2023-06-04 14:41:47 +0700

    Refactor Subject model

--- src/api_entity/Subject/model.py
+++ src/api_entity/Subject/model.py
@@ -30,31 +30,17 @@ class SubjectModel_create_for_response(BaseModel):
     description: Optional[constr(min_length=0, max_length=150)] = Field(description="описание предмета")
     teacher: dict = Field(..., description="информация об учителе")
 
-    @validator('teacher', pre=True)
+    @validator('teacher')
     def teacher_is_dict(cls, v):
-        assert len(v) != 0, "Словарь teacher пуст"
-        try:
-            SubjectModel_create_for_response._validate_teacher(v)
-        except ValueError as e:
-            raise ValueError(
-                f"Error in func SubjectModel_create_for_response:teacher_is_dict" +
-                f"Ошибка при валидации типа поля teacher. Исходное значение: {v}"
-            ) from e
-        return v
-    @staticmethod
-    @validate_arguments  # валидируем тип функции с помощью специальной функции pydantic
-    def _validate_teacher(v: Optional[Dict[str, str]]):
-        pass
-    @validator('teacher', pre=True)
-    def check_teacher_dict(cls, v):
+        assert v != {}, "Словарь teacher пуст"
         try:
             TeacherModel_create_for_response.parse_obj(v)
         except ValueError as e:
-            raise ValueError(f"Error in func SubjectModel_create_for_response:teacher") from e
+            raise ValueError(f"Error in func SubjectModel_create_for_response:teacher dict") from e
         return v
 
     @validator("subject_id")
-    def subject_id_check(cls, v:str):
+    def subject_id_check(cls, v: str):
         assert v.startswith("SB"), "student_id начинается не с ST"
         try:
             v_list = v.split("SB")
@@ -63,28 +49,43 @@ class SubjectModel_create_for_response(BaseModel):
             raise ValueError(f"Error in func SubjectModel_create_for_response:subject_id") from e
         return v
 
+
 class SubjectModel_update_for_factory(BaseModel):
     """Модель для response"""
     name: Optional[constr(min_length=1, max_length=50)] = Field(description="название предмета")
     teacher_id: Optional[str] = Field(description="id-к учителя")
     description: Optional[constr(min_length=0, max_length=150)] = Field(description="описание предмета")
 
-class SubjectsModel_get_for_response(SubjectModel_create_for_response):
+
+class SubjectModel_update_for_response(SubjectModel_create_for_response):
     pass
 
 
-if __name__ == "__main__":
+class SubjectModel_get_for_response(SubjectModel_create_for_response):
+    pass
 
+
+class SubjectModel_delete_for_response(BaseModel):
+    message: str = Field(description="сообщение об успехе удаления")
+
+    @validator("message")
+    def message_check(cls, v: str):
+        assert "You have successfully deleted the subject with the following ID: SB" in v, f"Wrong message for delete"
+        return v
+
+
+if __name__ == "__main__":
     body = {
-    "subject_id": "SB210",
-    "name": "Литература_upd",
-    "description": "new",
-    "teacher": {
-        "staff_id": "TC898",
-        "first_name": "teacher_name_02",
-        "last_name": "teacher_last_name_2",
-        "email_address": "teacher_2@mail.ru"
+        "subject_id": "SB210",
+        "name": "Литература_upd",
+        "description": "new",
+        "teacher": {
+            "staff_id": "TC898",
+            "first_name": "teacher_name_02",
+            "last_name": "teacher_last_name_2",
+            "email_address": "teacher_2@mail.ru"
+        }
     }
-}
+    body_2 = {"message": "You have successfully deleted the subject with the following ID: SB491"}
     subject_1 = SubjectModel_create_for_response.parse_obj(body)
     pass
