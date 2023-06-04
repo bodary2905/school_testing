042c5df2e0737c665879b99cca311d01eb0e1e65
Boyarkina Darya <bo_dary@mail.ru>
2023-06-04 12:47:48 +0700

    Add student_getItems

---
+++ scripts/user_create/student_getItems.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import random
+
+api_url = "http://127.0.0.1:5000/api/v1/students"
+
+with open("tokens.json", "r") as file:
+    # получаем словарь с токенами
+    tokens = json.load(file)
+    # получаем случайный токен из словаря tokens
+    token = random.choice(list(tokens.values()))
+    # формируем хэдере авторизации
+    auth = {"Authorization": token}
+    with open("students_update.json", "r") as file:
+        response = requests.get(api_url, headers=auth)
+        # проверяем статус код
+        assert response.status_code == 200, "Wrong status_code during student_get"
+        body = response.json()
+        print(body)
+
+if __name__ == "__main__":
+    import os
+
+    filename = os.path.basename(__file__)
+    print(f"{filename} worked success!")
