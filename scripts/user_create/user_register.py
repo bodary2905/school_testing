c35676bc4af0d1c16f0c03069834ed0b00b5d265
Boyarkina Darya <bo_dary@mail.ru>
2023-06-05 13:37:53 +0700

    Refactoring

--- scripts/user_create/user_register.py
+++ scripts/user_create/user_register.py
@@ -1,18 +1,21 @@
 import requests
 import json
 
-api_url = "http://127.0.0.1:5000/api/v1/auth/register"
-# открываем json-файл
+from const import api_url_auth
+
+# открываем json-файл с юзерами
 with open("json_files/users.json", "r") as file:
     # получаем словарь с юзерами
     users = json.load(file)
-    # через цикл for создаем user-ов
+    # через цикл for регистрируем user-ов
     for user in users.values():
-        response = requests.post(api_url, json=user)
+        response = requests.post(f"{api_url_auth}register", json=user)
         assert response.status_code == 201, "Wrong status_code during register"
 
 if __name__ == "__main__":
     import os
 
+    # получаем имя файла user_register.py по указанному пути
     filename = os.path.basename(__file__)
+    # выводим сообщение об успехе
     print(f"{filename} worked success!")
