2badc1a465852e3531831c7a0d58aaafaba70fdf
Boyarkina Darya <bo_dary@mail.ru>
2023-06-06 14:30:32 +0700

    Add file for clear tables

--- src/db_client.py
+++ src/db_client.py
@@ -33,21 +33,35 @@ def execute_read_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
+def execute_query(connection, query):
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+    except Exception as e:
+        print(f"The error '{e}' occurred")
+
+
 if __name__ == "__main__":
     import os
     from src import config
-    from src.const import EnvName
 
     db_credential = (
-        os.getenv(EnvName.DB_NAME1),
-        os.getenv(EnvName.DB_USER),
-        os.getenv(EnvName.DB_PASSWORD),
-        os.getenv(EnvName.DB_HOST),
-        int(os.getenv(EnvName.DB_PORT))
+        os.getenv("DB_NAME"),
+        os.getenv("DB_USER"),
+        os.getenv("DB_PASSWORD"),
+        os.getenv("DB_HOST"),
+        int(os.getenv("DB_PORT"))
     )
     connection = create_connection(*db_credential)
-    connection.set_session(readonly=True, autocommit=True)
-    select_users = "SELECT * FROM customers"
-    result = execute_read_query(connection, select_users)
-    print(result[3])
+    connection.set_session(readonly=False, autocommit=True)
+    # delete_student = "delete from students where email_address = 'student_01@mail.ru'"
+    # delete_students = "delete from students"
+    # result = execute_query(connection, delete_students)
+    select_students = "select * from students"
+    result = execute_read_query(connection, select_students)
+    print(result)
+    # select_users = "SELECT * FROM users"
+    # result = execute_read_query(connection, select_users)
+    # print(result[3])
     connection.close()
