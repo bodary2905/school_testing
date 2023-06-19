af5e0fcc833dfec275ca6421e1a921ffcc796f17
Boyarkina Darya <bo_dary@mail.ru>
2023-06-19 17:11:00 +0700

    Add test for subject

--- src/api_entity/Teacher/model.py
+++ src/api_entity/Teacher/model.py
@@ -67,6 +67,19 @@ class TeacherModel_get_for_response(TeacherModel_create_for_response):
     pass
 
 
+class TeacherModel_getItems_for_response(BaseModel):
+    teachers: list = Field(..., description="список учителей")
+
+    @validator("teachers")
+    def teachers_check(cls, v: list):
+        for _dict in v:
+            try:
+                TeacherModel_get_for_response.parse_obj(_dict)
+            except ValueError as e:
+                raise ValueError(f"Error in func StudentModel_create_for_factory:minors for teacher") from e
+        return v
+
+
 class TeacherModel_delete_for_response(BaseModel):
     message: str = Field(description="сообщение об успехе удаления")
 
@@ -84,6 +97,50 @@ if __name__ == "__main__":
         "staff_id": "TC123"
     }
     body_2 = {"message": "You have successfully deleted the teacher with the following ID: TC"}
-    response = TeacherModel_create_for_response.parse_obj(body)
+    body_3 = {
+        "teachers": [
+            {
+                "staff_id": "TC399",
+                "first_name": "teacher_name_1",
+                "last_name": "teacher_last_name_1",
+                "email_address": "teacher_1@mail.ru",
+                "created_at": "Thu, 08 Jun 2023 12:21:24 -0000",
+                "updated_at": None
+            },
+            {
+                "staff_id": "TC696",
+                "first_name": "teacher_name_2",
+                "last_name": "teacher_last_name_2",
+                "email_address": "teacher_2@mail.ru",
+                "created_at": "Thu, 08 Jun 2023 12:21:24 -0000",
+                "updated_at": None
+            },
+            {
+                "staff_id": "TC124",
+                "first_name": "teacher_name_3",
+                "last_name": "teacher_last_name_3",
+                "email_address": "teacher_3@mail.ru",
+                "created_at": "Thu, 08 Jun 2023 12:21:24 -0000",
+                "updated_at": None
+            },
+            {
+                "staff_id": "TC621",
+                "first_name": "teacher_name_4",
+                "last_name": "teacher_last_name_4",
+                "email_address": "teacher_4@mail.ru",
+                "created_at": "Thu, 08 Jun 2023 12:21:24 -0000",
+                "updated_at": None
+            },
+            {
+                "staff_id": "TC273",
+                "first_name": "teacher_name_5",
+                "last_name": "teacher_last_name_5",
+                "email_address": "teacher_5@mail.ru",
+                "created_at": "Thu, 08 Jun 2023 12:21:24 -0000",
+                "updated_at": None
+            }
+        ]
+    }
+    response = TeacherModel_getItems_for_response.parse_obj(body_3)
     print(response)
     pass
