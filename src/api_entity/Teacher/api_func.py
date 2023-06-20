8bdb3560992d2c2aebd4d78403a181029613934f
Boyarkina Darya <bo_dary@mail.ru>
2023-06-20 17:14:44 +0700

    Refactoring text erroro after delete

--- src/api_entity/Teacher/api_func.py
+++ src/api_entity/Teacher/api_func.py
@@ -112,5 +112,5 @@ if __name__ == "__main__":
     TeacherApiFunc.delete(teacher_id=teacher_id_create, headers=auth)
     # Проверяем, что удаленный учитель отсутствует через send_get
     response = send_get(url=TeacherFullPath.get.value / teacher_id_create, headers=auth)  # в kwargs передаем headers
-    assert response.status_code == 404, f"Wrong status_code {entity_name}:get"
+    assert response.status_code == 404, f"Wrong status_code {entity_name}:send_get after delete"
     pass
--- tests/teacher/test_crud_teacher.py
+++ tests/teacher/test_crud_teacher.py
@@ -56,5 +56,5 @@ def test_crud_teaher(user1_auth_hearders):
     # Проверяем, что удаленный учитель отсутствует через send_get
     response = send_get(url=TeacherFullPath.get.value / teacher_id_create,
                         headers=user1_auth_hearders)  # в kwargs передаем headers
-    assert response.status_code == 404, f"Wrong status_code {entity_name}:get"
+    assert response.status_code == 404, f"Wrong status_code {entity_name}:send_get after delete"
     pass
