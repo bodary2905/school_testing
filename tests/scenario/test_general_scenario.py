978b9da0c17b60918b267b8016fdb23998dcf2cb
Boyarkina Darya <bo_dary@mail.ru>
2023-06-21 10:27:12 +0700

    Add assert for test_general_scenario.py

--- tests/scenario/test_general_scenario.py
+++ tests/scenario/test_general_scenario.py
@@ -30,7 +30,6 @@ def test_general_scenario(user1_auth_hearders):
                                                                  headers=user1_auth_hearders)  # create возвращает body и model
     # получаем id-к созданного учителя через модель
     id_teacher = teacher_model_create.staff_id
-    print(id_teacher)
     # Создаем предметы
     # Создаем мажорный и минорный предмет с помощью фабрики и назначаем на них учителя
     major_subject = SubjectFactory_create.build(teacher_id=id_teacher)
@@ -43,9 +42,36 @@ def test_general_scenario(user1_auth_hearders):
     # получаем id-к созданных мажорных и минорных предметов
     id_major = major_subject_model_create.subject_id
     id_minor = minor_subject_model_create.subject_id
+    # получаем созданный мажорный и минорный предмет с помощью api_func через метод get
+    major_get_create, major_model_get_create = SubjectApiFunc.get(subject_id=id_major,
+                                                                  headers=user1_auth_hearders)
+    minor_get_create, minor_model_get_create = SubjectApiFunc.get(subject_id=id_minor,
+                                                                  headers=user1_auth_hearders)
+    # получаем id-к учителя
+    id_teacher_major = major_model_get_create.teacher["staff_id"]
+    id_teacher_minor = minor_model_get_create.teacher["staff_id"]
+    # сравниваем id-ки учителей мажорного и миноргного предметов (должны быть одинаковыми,
+    # так как на мажорный и минорный предметы назначался один учитель)
+    assert id_teacher_major == id_teacher_minor, f"id major {id_teacher_major} not equal id minor {id_teacher_minor}"
+    # сравниваем отправленный id-к и полученный в предемете id-к учителя
+    assert id_teacher == id_teacher_major, f"id teacher {id_teacher} not equal id teaher in major subject {id_teacher_major}"
+    assert id_teacher == id_teacher_minor, f"id teacher {id_teacher} not equal id teaher in major subject {id_teacher_minor}"
     # Создаем Студента
     # Создаем студента с помощью фабрики и присваиваем ему созданные предметы
     student_factory_create = StudentFactory_create.build(major_id=id_major, minors=id_minor)
     # создаем студента с помощью api_func через метод create
     student_create, student_model_create = StudentApiFunc.create(student_factory_create.dict(),
-                                                                 headers=user1_auth_hearders)  # create возвращает body и model
+                                                                 headers=user1_auth_hearders)
+    # получаем id-к студента через модель
+    student_id_create = student_model_create.student_id
+    # получаем созданного студента с помощью api_func через метод get
+    student_get_create, student_model_get_create = StudentApiFunc.get(student_id=student_id_create,
+                                                                      headers=user1_auth_hearders)
+    # получаем id-к мажорных и минорных предметов из созданного студента
+    id_major_student = student_model_get_create.major["subject_id"]
+    id_minor_student = student_model_get_create.minors[0]["subject_id"]
+    # сравниваем полученные id-ки с отправленными id-ми мажорного и минорного предметов
+    assert id_major == id_major_student, f"id major {id_major} not equal id major subject in student {id_major_student}"
+    assert id_minor == id_minor_student, f"id minor {id_minor} not equal id minor subject in student {id_minor_student}"
+    # проверяем, что мажорный предмет НЕ равеен минорному предмету
+    assert id_major_student != id_minor_student, f"id major {id_major_student} equal id minor subject {id_minor_student}"
